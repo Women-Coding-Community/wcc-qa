84259a0e20966e71279e657e57872f894865ee4a
nora-weisser <eleo.belova@yandex.ru>
2026-05-25 20:05:06 +0200

    feat: removed unnecessary files

--- helpers/fixtures/pages.fixture.ts
+++ helpers/fixtures/pages.fixture.ts
@@ -1,19 +1,13 @@
 import { test as base } from '@playwright/test';
 import { AdminHomePage } from '@pages/admin/home.page';
-import { WebsiteHomePage } from '@pages/website/home.page';
-import { WebsiteMentorshipPage } from '@pages/website/mentorship.page';
 import { Header } from '@pages/website/components/header';
-import { Footer } from '@pages/website/components/footer';
 
 export type AdminPages = {
   home: AdminHomePage;
 };
 
 export type WebsitePages = {
-  home: WebsiteHomePage;
-  mentorship: WebsiteMentorshipPage;
   header: Header;
-  footer: Footer;
 };
 
 export const adminPagesTest = base.extend<{ adminPages: AdminPages }>({
@@ -27,10 +21,7 @@ export const adminPagesTest = base.extend<{ adminPages: AdminPages }>({
 export const websitePagesTest = base.extend<{ websitePages: WebsitePages }>({
   websitePages: async ({ page }, use) => {
     await use({
-      home: new WebsiteHomePage(page),
-      mentorship: new WebsiteMentorshipPage(page),
       header: new Header(page),
-      footer: new Footer(page),
     });
   },
 });
